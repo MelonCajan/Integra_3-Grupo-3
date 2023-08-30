a320c23c7abc29ab49eacb56cc6df9b3c952764c
Alvaro Valenzuela <brawnylol@outlook.cl>
2023-08-30 14:54:31 -0400

    a

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TextComponent } from 'react-native';
 import App_button from './component/button';
 import HomeScreen from './component/inicio';
 import DetailsScreen from './component/navbar';
@@ -12,6 +12,8 @@ export default function App() {
       <StatusBar style="auto" />
       <HomeScreen />
       <DetailsScreen />
+      <TextComponent 
+      hola ></TextComponent>
       <Image source={require('./assets/asesoria1.png')} style={{ width: '50%', height: '15%', borderTopLeftRadius: 25, borderTopRightRadius: 25 }} />
       <App_button />
     </View>
